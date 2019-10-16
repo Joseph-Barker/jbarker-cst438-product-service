1989dbf0a40921923bb9f3194c768845443d05eb
Joseph <jbarker@csumb.edu>
2019-10-15 19:04:20 -0700

    added swagger

--- productdb/src/main/java/edu/csumb/cst438/productdb/service/ProductService.java
+++ productdb/src/main/java/edu/csumb/cst438/productdb/service/ProductService.java
@@ -1,5 +1,14 @@
 package edu.csumb.cst438.productdb.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.csumb.cst438.productdb.repositores.IProductRepository;
+
+@Service
 public class ProductService {
+    @Autowired
+    IProductRepository productRepository;
+
 
 }
